bf2caf844b1649b7ccf0e6034685df89766b51bc
AysanAghazadeh <aya34@pitt.edu>
2025-03-13 05:52:04 -0400

    changed

--- VLMs/VLM.py
+++ VLMs/VLM.py
@@ -1,18 +1,16 @@
 from torch import nn
-from VLMs.LLAVA import LLAVA
 from VLMs.InternVL2 import InternVL
 from VLMs.LLAVA16 import LLAVA16
-from VLMs.InstructBLIP import InstructBLIP
+from VLMs.QWenVL import QWenVL
 
 
 class VLM(nn.Module):
     def __init__(self, args):
         super(VLM, self).__init__()
         model_map = {
-            'LLAVA': LLAVA,
+            'QWenVL': QWenVL,
             'LLAVA16': LLAVA16,
-            'InternVL': InternVL,
-            'InstructBLIP': InstructBLIP
+            'InternVL': InternVL
         }
         self.model = model_map[args.VLM](args)
 
