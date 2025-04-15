ca3dea889d7259e4198146f44538a5be16475c78
AysanAghazadeh <aya34@pitt.edu>
2025-04-15 11:28:50 -0400

    changed

--- VLMs/InternVL2.py
+++ VLMs/InternVL2.py
@@ -12,10 +12,10 @@ class InternVL(nn.Module):
         self.args = args
         self.model = AutoModel.from_pretrained(
             "OpenGVLab/InternVL2-4B",
-            torch_dtype=torch.bfloat16,
-            load_in_8bit=True,
+            # torch_dtype=torch.bfloat16,
+            # load_in_8bit=True,
             low_cpu_mem_usage=True,
-            device_map='auto',
+            device_map={'': 2},
             trust_remote_code=True).eval()
         self.tokenizer = AutoTokenizer.from_pretrained("OpenGVLab/InternVL2-26B",
                                                        trust_remote_code=True)
