2c5c948d328764cb06acfdbb81800279ba30be7d
AysanAghazadeh <aya34@pitt.edu>
2025-05-13 01:59:47 -0400

    changed

--- bias_evaluation_deepface.py
+++ bias_evaluation_deepface.py
@@ -3,7 +3,6 @@ import cv2
 import json
 import os
 import pandas as pd
-from configs.inference_config import get_args
 
 image_analysis = {}
 def analyze_image(image_path, image_url):
@@ -40,15 +39,14 @@ def analyze_image(image_path, image_url):
 
 
 if __name__ == "__main__":
-    args = get_args()
     analyze_image(args)
-    image_path_list = pd.read_csv(os.path.join(args.results_path, 'AR_Flux_20250508_202121.csv')).generated_image_url.values
-    ref_path_list = pd.read_csv(os.path.join(args.results_path, 'AR_Flux_20250508_202121.csv')).generated_image_url.values
+    image_path_list = pd.read_csv(os.path.join('../experiments/results', 'AR_Flux_20250508_202121.csv')).generated_image_url.values
+    ref_path_list = pd.read_csv(os.path.join('../experiments/results', 'AR_Flux_20250508_202121.csv')).generated_image_url.values
     for row in image_path_list:
         image_url = row[0]
         if image_url not in ref_path_list:
             continue
         image_path = row[1]
         analyze_image(image_path, image_url)
-        with open(os.path.join(args.results_path, 'deepface_analysis_FLUX.json'), "w") as f:
+        with open(os.path.join('../experiments/results', 'deepface_analysis_FLUX.json'), "w") as f:
             json.dump(image_analysis, f)
