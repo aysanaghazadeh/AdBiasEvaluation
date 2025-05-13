5c405440a5c5c1b102b011b91dd89e14de9df332
AysanAghazadeh <aya34@pitt.edu>
2025-05-13 14:50:04 -0400

    changed

--- T2I_models/Custome_SD3.py
+++ T2I_models/Custome_SD3.py
@@ -73,7 +73,8 @@ class CustomeSD3(nn.Module):
             load_in_8bit=True,
         ).to(self.args.device)
         self.projection_block = ProjectionBlock(args)
-        # self.projection_block.load_state_dict(torch.load("SD3_finetuned_projection_only/checkpoint-2000/projection_block.pt"))
+        if not args.train and args.fine_tuned:
+            self.projection_block.load_state_dict(torch.load(os.path.join(args.model_path, "SD3_modified/checkpoint-7500/projection_block.pt")))
         self.projection_block.to(self.args.device) 
         self.pipeline.projection_block = self.projection_block
         self.country_image_map = json.load(open(os.path.join(args.data_path, "train/countries_image_map.json")))
