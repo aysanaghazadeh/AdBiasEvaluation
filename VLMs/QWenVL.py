bb2f6864409940ece56d38c265d49f2f4f50c31c
AysanAghazadeh <aya34@pitt.edu>
2025-05-13 00:41:57 -0400

    changed

--- VLMs/QWenVL.py
+++ VLMs/QWenVL.py
@@ -1,4 +1,4 @@
-from transformers import Qwen2VLForConditionalGeneration, AutoTokenizer, AutoProcessor
+from transformers import Qwen2_5_VLForConditionalGeneration, AutoTokenizer, AutoProcessor
 from qwen_vl_utils import process_vision_info
 import torch
 import os
@@ -7,9 +7,9 @@ class QWenVL(torch.nn.Module):
     def __init__(self, args):
         super().__init__()
         self.args = args
-        self.model = Qwen2VLForConditionalGeneration.from_pretrained("Qwen/Qwen2.5-VL-32B-Instruct")
+        self.model = Qwen2_5_VLForConditionalGeneration.from_pretrained("Qwen/Qwen2.5-VL-32B-Instruct")
         self.model = self.model.to('cuda')
-        self.processor = AutoProcessor.from_pretrained("Qwen/Qwen2-VL-7B-Instruct")
+        self.processor = AutoProcessor.from_pretrained("Qwen/Qwen2.5-VL-32B-Instruct")
 
 
     def forward(self, images, prompt, generate_kwargs={'max_new_tokens': 20}):
