2d9ae2465d364af7d9fa259b1c25cb82de1d5101
AysanAghazadeh <aya34@pitt.edu>
2025-05-15 02:00:59 -0400

    changed

--- T2I_models/Custome_SD3.py
+++ T2I_models/Custome_SD3.py
@@ -47,7 +47,7 @@ class ProjectionBlock(torch.nn.Module):
             # print(encoded_prompt.size(), cultural_components_reason.size())
             # print(torch.cat([encoded_prompt, cultural_components_reason], dim=1).size())
             # return encoded_cultural_components
-            return torch.cat([encoded_prompt, encoded_cultural_components], dim=1)
+            return torch.cat([encoded_cultural_components, encoded_prompt], dim=1)
         encoded_prompt = encoded_prompt.to(self.args.device)
         inputs = self.CLIP_processor(images=image, return_tensors="pt").to(self.args.device)
         clip_image_features = self.CLIP_model.get_image_features(**inputs)
