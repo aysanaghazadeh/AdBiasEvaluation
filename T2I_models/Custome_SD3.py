87bd9bd36350367afc129d4c63c9bbc105fa2c8b
AysanAghazadeh <aya34@pitt.edu>
2025-05-15 05:29:31 -0400

    changed

--- T2I_models/Custome_SD3.py
+++ T2I_models/Custome_SD3.py
@@ -55,7 +55,7 @@ class ProjectionBlock(torch.nn.Module):
                     )
         # print(features)
         
-        features = torch.cat([encoded_prompt, encoded_cultural_components, features], dim=1)
+        features = torch.cat([encoded_prompt, features], dim=1)
         features = features.to(self.args.device)
         return features
 
