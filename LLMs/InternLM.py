453009e07d2259960d72eaa8d2e2c6dc04c458a9
AysanAghazadeh <aya34@pitt.edu>
2025-05-18 16:41:24 -0400

    changed

--- LLMs/InternLM.py
+++ LLMs/InternLM.py
@@ -19,23 +19,6 @@ class InternLM(nn.Module):
 
     def forward(self, prompt):
         if not self.args.train:
-            # messages = [
-            #     {"role": "user", "content": prompt},
-            # ]
             response, history = self.model.chat(self.tokenizer, prompt, history=[])
-            print(response)
             return response
-            # length = 0
-            # for response, history in self.model.stream_chat(self.tokenizer, prompt, history=[]):
-            #     output = history[0][-1]
-            #     length = len(response)
-            # return output
             
-            # inputs = self.tokenizer([prompt], return_tensors="pt")
-            # for k, v in inputs.items():
-            #     inputs[k] = v.cuda()
-            # gen_kwargs = {"temperature": 0.8, "do_sample": True}
-            # output = self.model.generate(**inputs, **gen_kwargs)
-            # output = self.tokenizer.decode(output[0].tolist(), skip_special_tokens=True)
-            # return output
-
