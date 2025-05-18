5c72dbc4cac48aab355033a82bc1d447acd7d647
AysanAghazadeh <aya34@pitt.edu>
2025-05-17 20:08:39 -0400

    changed

--- LLMs/GPT4o.py
+++ LLMs/GPT4o.py
@@ -9,14 +9,12 @@ class GPT4o(nn.Module):
     def __init__(self, args):
         super(GPT4o, self).__init__()
         self.args = args
-        os.environ["OPENAI_API_KEY"] = args.api_key
         self.client = OpenAI()
 
     def forward(self, prompt):
         completion = self.client.chat.completions.create(
             model="gpt-4o",
             messages=[
-                {"role": "developer", "content": "You are a helpful assistant."},
                 {
                     "role": "user",
                     "content": prompt
