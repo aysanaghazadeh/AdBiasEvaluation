566dcaf3f4c1dffd2b16709e0d781bd9ece91672
AysanAghazadeh <aya34@pitt.edu>
2025-05-19 04:29:51 -0400

    changed

--- T2I_models/Custome_SD3.py
+++ T2I_models/Custome_SD3.py
@@ -37,7 +37,7 @@ class ProjectionBlock(torch.nn.Module):
         if time_step < 20:
             # print(encoded_prompt.size(), cultural_components_reason.size())
             # print(torch.cat([encoded_prompt, cultural_components_reason], dim=1).size())
-            return torch.cat([encoded_prompt, encoded_cultural_components], dim=1)
+            return torch.cat([encoded_cultural_components, encoded_prompt], dim=1)
         encoded_prompt = encoded_prompt.to(self.args.device)
         inputs = self.CLIP_processor(images=image, return_tensors="pt").to(self.args.device)
         clip_image_features = self.CLIP_model.get_image_features(**inputs)
@@ -52,7 +52,7 @@ class ProjectionBlock(torch.nn.Module):
                     )
         # print(features)
         
-        features = torch.cat([features, encoded_prompt], dim=1)
+        features = torch.cat([features, encoded_cultural_components, encoded_prompt], dim=1)
         features = features.to(self.args.device)
         return features
 
