bc51f2eeda701a3992bc917270c3676d7b0a3e33
AysanAghazadeh <aya34@pitt.edu>
2025-05-19 04:39:42 -0400

    changed

--- cultural_image_generation.py
+++ cultural_image_generation.py
@@ -19,13 +19,12 @@ if __name__ == "__main__":
     countries = ['france', 'china', 'united arab emirates', 'south africa', 'mexico']
     country_short = ['fr', 'cn', 'uae', 'sa', 'm']
     for i, AR in ARs.items():
-        if str(i) != '14':
+        if str(i) != '2':
             continue
         for j, country in enumerate(countries):
             
             ct = country_short[j]
-            if ct != 'uae':
-                continue
+            
             print('prompt:', AR)
             print(country)
             prompt = f'''Generate an advertisement image that targets people from {country} conveying the following messages: \n
